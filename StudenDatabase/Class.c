17b268e46f8dfe9e9ce41cc695b66b78fa6fc42c
x <x@10.52.229.86>
2016-11-22 14:35:00 +0100

    DO IT!

--- StudenDatabase/Class.c
+++ StudenDatabase/Class.c
@@ -3,9 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-int main(){
+typedef struct Class{
     
-    printf("Hello");
-    return 0;
-}
+}Class;
