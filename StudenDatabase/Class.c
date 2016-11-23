ccc238bd3199c115ed09bffe071a29ec56066d0a
x <x@10.52.229.86>
2016-11-23 18:57:26 +0100

    Merge branch 'master' of https://github.com/JConstantin1/StudentDatabase
    # Conflicts:
    #	StudenDatabase/Class.c

--- StudenDatabase/Class.c
+++ StudenDatabase/Class.c
@@ -1,8 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-typedef struct Class{
-    
-}Class;
+#include "Class.h"
+
+Class* AddClass(int a, int b, char *d)
+{
+	Class *c;
+	c = (Class*)malloc (sizeof(Class));
+	(*c).id = a;
+	strcpy(c->name, d);
+	//(*s).name = c;
+	c -> semester = b;
+	return c;
+}
