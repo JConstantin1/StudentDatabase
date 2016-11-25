9b7748d73a2621af1d853aa1e8f2427338932f8c
Konstantin <Konstantin@10.52.224.47>
2016-11-25 14:37:52 +0100

    Merge branch 'master' of https://github.com/JConstantin1/StudentDatabase
    # Conflicts:
    #	StudenDatabase/nbproject/Makefile-Debug.mk
    #	StudenDatabase/nbproject/Makefile-Release.mk
    #	StudenDatabase/nbproject/configurations.xml
    #	StudenDatabase/nbproject/private/private.xml

--- StudenDatabase/DatabaseHandler.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-
-char selectDatabase(int a);
-int enterDatabase(char* a);
--- StudenDatabase/Student.h
+++
@@ -1,7 +0,0 @@
-    
-typedef struct student {
-	char name[50];
-	int student_nr;
-}Student;
-
-Student* AddStudent(int a, int b, char *c);
